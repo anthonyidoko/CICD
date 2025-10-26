adc37b626cdb9f2361b6f9793b1c618e11071b48
anthonyidoko <idokoanthony86@yahoo.com>
2025-10-26 23:53:01 +0100

    Disabled Lint Check for NullSafeMutableLiveData

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    lint {
+        disable.add("NullSafeMutableLiveData")
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
